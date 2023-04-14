2980f600345bb520e2cc1306eeac1203d172de74
alesukhin <alesukhin@museumplus.ru>
2023-04-14 11:13:31 +0300

    add music and tickets pagse

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,8 @@ import Swiper, {Autoplay, EffectCoverflow, EffectFade, Navigation, Pagination} f
 import './modules/attention';
 import '../components/accordion/js/index';
 import { Fancybox } from "@fancyapps/ui/dist/fancybox/fancybox.esm.js";
-import{ readMoreLess } from './modules/read-more'
+import{ readMoreLess } from './modules/read-more';
+import Plyr from 'plyr';
 
 document.addEventListener('DOMContentLoaded', () => {
   initMenu();
@@ -82,4 +83,77 @@ document.addEventListener('DOMContentLoaded', () => {
   const alc_date = new Date();
   alc_date.setDate(alc_date.getDate() + 1);
   document.cookie = "alertcookie=1;path=/;expires=" + alc_date.toGMTString();
+
+  const radio = new Plyr(document.querySelector('.plyr'), {
+    iconUrl: '/images/sprite_auto.svg',
+    controls: ['play', 'progress', 'current-time', 'mute', 'volume']
+  });
+
+
+  const audioPlayer = document.querySelector('.playlist');
+
+  if (audioPlayer) {
+    const songs = audioPlayer.querySelectorAll('.playlist__list li');
+    let i;
+    let active = null;
+
+    for(i = 0; i < songs.length; i++) {
+      songs[i].onclick = changeChannel;
+    }
+
+    setSource( getId(songs[0]), buildSource(songs[0]) );
+
+    document.querySelector('.plyr').addEventListener('ended', nextSong);
+
+    function changeChannel(e) {
+      setSource( getId(e.target), buildSource(e.target), true );
+    }
+
+    function getId(el) {
+      return Number(el.getAttribute('data-id'));
+    }
+
+    function buildSource(el) {
+      var obj = [{
+        src: el.getAttribute('data-audio'),
+        type: 'audio/mp3'
+      }];
+
+      return obj;
+    }
+
+    function setSource(selected, sourceAudio, play) {
+      if(active !== selected) {
+        active = selected;
+        radio.source = {
+          type: 'audio',
+          title: 'test',
+          sources: sourceAudio
+        };
+
+        for(var i = 0; i < songs.length; i++) {
+          if(Number(songs[i].getAttribute('data-id')) === selected) {
+            songs[i].className = 'playlist__item active';
+          } else {
+            songs[i].className = 'playlist__item';
+          }
+        }
+
+        if(play) {
+          radio.play();
+        }
+      } else {
+        radio.togglePlay();
+      }
+    }
+
+    function nextSong(e) {
+      var next = active + 1;
+
+      if(next < songs.length) {
+        setSource( getId(songs[next]), buildSource(songs[next]), true );
+      }
+    }
+  }
+
 })
